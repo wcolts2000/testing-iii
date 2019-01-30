ac122dad9727110f892a2d65d66d7398f35051cb
wcolts2000 <wcolts2000@gmail.com>
2019-01-30 13:00:29 -0800

    initial commit, first test on Display to show rending unlocked and open complete

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,19 @@
 // Test away!
+import "jest-dom/extend-expect";
+import React from "react";
+import { render } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
+import Display from "./Display";
+
+describe("<Display />", () => {
+  describe("when component first renders, should be in open and unlocked state", () => {
+    it("renders the child divs in unlocked and open state", async () => {
+      const { getByTestId } = await render(<Display />);
+      const lock = getByTestId("lock");
+      const gate = getByTestId("gate");
+
+      expect(lock).toHaveTextContent(/unlocked/i);
+      expect(gate).toHaveTextContent(/open/i);
+    });
+  });
+});
